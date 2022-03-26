76ff0beb93eb12ae2cd9c2f0c9adfffae019ddc5
Nikola <woleet@outlook.com>
2022-03-26 23:15:35 +0100

    Layout link and DRY

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def show
-    @article = Article.find(params[:id])
   end
 
   def index
@@ -12,7 +13,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(permit_params)
     if @article.save
       flash[:notice] = "The article has been created"
       redirect_to @article
@@ -22,12 +23,10 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(permit_params)
       flash[:notice] = 'Article has been updated'
       redirect_to @article
     else
@@ -36,8 +35,18 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
   end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def permit_params
+    params.require(:article).permit(:title, :description)
+  end
+
 end
