749aad995b215ca118fbe3f276001808c0e5ad2a
Nikola <woleet@outlook.com>
2022-03-28 19:34:56 +0200

    Show user, user image and user articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
 
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles # Instead of adding obj in render
+  end
+
   def new
     @user = User.new
   end
