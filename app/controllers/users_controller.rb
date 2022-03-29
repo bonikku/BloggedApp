c8c15c454e8a131ef1efbd65fe454006d5832969
Nikola <woleet@outlook.com>
2022-03-29 22:04:47 +0200

    Delete user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show edit update]
+  before_action :set_user, only: %i[show edit update destroy]
   before_action :require_user, only: %i[edit update]
-  before_action :require_same_user, only: %i[edit update]
+  before_action :require_same_user, only: %i[edit update destroy]
 
   def show
     @articles = @user.articles.paginate(page: params[:page], per_page: 5) # Instead of adding obj in render
@@ -37,6 +37,13 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = 'Account and all associated articles has been permanently deleted'
+    redirect_to root_path
+  end
+
   private
 
   # Whitelist params
@@ -52,7 +59,7 @@ class UsersController < ApplicationController
     return if current_user == @user
 
     flash[:alert] = 'You can only edit your own account!'
-    redirect_to @user
+    redirect_to login_path
   end
 
 end
